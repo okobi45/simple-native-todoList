9d1212b71736b81cedffbeb0116f6b190cbc76f2
okobi45 <okobieigbe@gmail.com>
2023-03-06 12:48:48 +0100

    Added Component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskManger}></View>
+      <Text style={styles.mainTitle}>Daily Task</Text>
+      <View style={styles.items}>
+      </View>
     </View>
   );
 }
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskManger: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  mainTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+
   },
 });
